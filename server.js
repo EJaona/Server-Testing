c5cbf53cc80a13613c482814a717b20409bea920
Enoka Jaona <Ejaona@gmail.com>
2019-03-07 17:13:17 -0600

    added end points for get, post and delete

--- server.js
+++ server.js
@@ -5,4 +5,35 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/", async (req, res) => {
+  try {
+    const test = db("test");
+    res.status(200).json({ test });
+  } catch (error) {
+    res.status(500).json({ message: "problem getting" });
+  }
+});
+
+server.post("/", async (req, res) => {
+  try {
+    const data = req.body;
+    const test = db("test").insert(data);
+    res.status(201).json({ test });
+  } catch (error) {
+    res.status(500).json({ message: "can't post" });
+  }
+});
+
+server.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const test = db("test")
+      .remove()
+      .where({ Id: id });
+    res.status(200).json({ message: "successfully deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "can't delete" });
+  }
+});
+
 module.exports = server;
